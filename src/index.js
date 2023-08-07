2bf2fa6f4afc15aa5e10befef0ac0a960baa20f4
Dreamkaman <riabushko@gmail.com>
2023-08-07 22:47:11 +0300

    Added loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./index.html"; //for 'html-loader'
+import "./index.scss"; //for 'mini-css-extract-plugin'
+
+import "./scripts/changeMainTitle.js";
 
 console.log("Hallo world!");
---
+++ src/scripts/changeMainTitle.js
@@ -0,0 +1,3 @@
+const h1 = document.getElementsByTagName("h1");
+
+console.log(h1);
