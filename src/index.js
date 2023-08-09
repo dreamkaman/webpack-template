49c489ea247c76e2eeaf8ab41a43b9a28ce23923
Dreamkaman <riabushko@gmail.com>
2023-08-09 14:22:42 +0300

    Added corrections to package.json, webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 // import index files block
-import "./index.html"; //for 'html-loader'
-import "./index.scss"; //for 'mini-css-extract-plugin'
-
-// import scripts block
-import "./scripts/changeMainTitle.js";
-
-// example of image insertion start
-import avatar from "./images/avatar.png";
-
-const image = new Image();
-image.src = avatar;
-image.alt = "avatar";
-
-const imageDiv = document.querySelector(".img-wrapper");
-imageDiv.append(image);
-// example of image insertion finish
+import './index.html'; //for 'html-loader'
+import './partials/header.html';
+import './partials/footer.html';
+import './partials/hero.html';
+import './partials/section-about.html';
+import './partials/section-contact-us.html';
+import './partials/section-our-projects.html';
+import './partials/section-our-tasks.html';
+import './index.scss'; //for 'mini-css-extract-plugin'
