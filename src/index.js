171e610130f3cd2afeb0706a5a57e2503d5446dd
Dreamkaman <riabushko@gmail.com>
2023-08-25 16:05:12 +0300

    Added corrections. Cleared structure

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import './index.html'; //for 'html-loader'
 import './partials/header.html';
 import './partials/footer.html';
 import './partials/hero.html';
-import './partials/section-about.html';
-import './partials/section-contact-us.html';
-import './partials/section-our-projects.html';
-import './partials/section-our-tasks.html';
+import './partials/section-1.html';
 import './index.scss'; //for 'mini-css-extract-plugin'
